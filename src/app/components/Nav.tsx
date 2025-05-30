18f4a1cbfc0285fadf683f2e0bf4561bb40476ff
EnochArmah-Quaye <armahquayeenoch494@gmail.com>
2025-05-30 09:46:47 +0000

    Still working on the navbar
    Almost there

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -4,9 +4,9 @@ const nav = ["00 Home ", " 01 Destination ", " 02 Crew", " 03 Technology"];
 
 function Nav() {
   return (
-    <div className="flex pt-5">
+    <div className="flex pt-[40px]">
         <Image src="/logo.svg" alt="" width={30} height={30} className="ml-8 mr-auto mb-10"/>
-        <ul className="flex mr-10">
+        <ul className="flex mr-10 backdrop-blur-md pt-5 ">
             {nav.map((item,index)=> (
                 <li key={index} className="pl-10">{item}</li>
             ))}
